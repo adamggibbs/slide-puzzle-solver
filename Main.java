1c29067aa2d6c57300923934729c3360599bfa1e
Adam Gibbs 22 <agibbs22@remus.amherst.edu>
2021-05-07 16:41:41 -0400

    Changes to main.

--- Main.java
+++ Main.java
@@ -2,12 +2,19 @@
 public class Main {
     public static void main(String[] args){
         
+	// create a puzzle
         Puzzle puzzle = new Puzzle(5,5);
+
+	// shuffle the puzzle
         puzzle.shuffle();
         puzzle.print();
+
+	// solve the puzzle
         puzzle = Solver.solve(puzzle);
         puzzle.print();
         Solver.printSolution(puzzle);
+
+	System.out.println();
         
     }
 }
